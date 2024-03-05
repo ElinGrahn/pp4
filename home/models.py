824c9b62a24f3314feedce407c1cb0defb99a3ed
Elin Grahn <elli.grahn@live.se>
2024-03-05 13:43:19 +0000

    removed imageField and argument-uniqe for errors

--- home/models.py
+++ home/models.py
@@ -4,11 +4,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 # Model for posting recipes
 class Recipe_Post(models.Model):
-    title = models.CharField(max_length=200, uniqe=True)
-    slug = models.SlugField(max_length=200, uniqe=True)
-    author = models.ForeignKey(User, on_delete.CASCADE)
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
     ingredients = models.TextField()
     instructions = models.TextField()
     description = models.TextField()
