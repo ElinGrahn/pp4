595b93e56fc1540c62190c924abbf02f7efee208
Elin Grahn <elli.grahn@live.se>
2024-03-07 16:41:36 +0000

    meta class in comment model

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Recipe_Post
+from .models import Comment
 
 # Register your models here.
 admin.site.register(Recipe_Post)
+admin.site.register(Comment)
--- home/models.py
+++ home/models.py
@@ -12,8 +12,14 @@ class Recipe_Post(models.Model):
     description = models.TextField()
 
 class Comment(models.Model):
-    post = models.ForeignKey(Recipe_Post, on_delete=CASCADE, related_name="comments")
-    user = models.ForeignKey(User, on_delete=CASCADE, related_name="commenter")
+    post = models.ForeignKey(Recipe_Post, on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
     name = models.CharField(max_length=50)
     body = models.TextField(max_length=200)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+    
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
