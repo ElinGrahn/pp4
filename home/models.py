27049d6f202a3a67928f9132aba8fc6f8b37baeb
Elin Grahn <elli.grahn@live.se>
2024-03-07 16:26:57 +0000

    making comment model in models.py

--- home/models.py
+++ home/models.py
@@ -10,3 +10,10 @@ class Recipe_Post(models.Model):
     ingredients = models.TextField()
     instructions = models.TextField()
     description = models.TextField()
+
+class Comment(models.Model):
+    post = models.ForeignKey(Recipe_Post, on_delete=CASCADE, related_name="comments")
+    user = models.ForeignKey(User, on_delete=CASCADE, related_name="commenter")
+    name = models.CharField(max_length=50)
+    body = models.TextField(max_length=200)
+    created_on = models.DateTimeField(auto_now_add=True)
