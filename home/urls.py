352f825e8e5756d199f8b18b3d7b83679ffb575d
Elin Grahn <elli.grahn@live.se>
2024-03-13 15:42:45 +0000

    bootstrap navbar in base, new templates

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('recipes/', recipes, name='recipes'),
+    path('favourites/', favourites, name='favourites'),
+    path('register/', register, name='register'),
+    path('login/', login, name='login'),
 ]
